a05a4ad78ac21fe3e6ab47fdb1a38279ec538d25
qxr <1091017109@qq.com>
2020-04-06 09:43:15 +0800

    LFU

---
+++ LineSweep/LFU.java
@@ -0,0 +1,112 @@
+package LineSweep;
+
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class LFU {
+    Map<Integer,Node> cacheMap;
+    Map<Integer,LinkedList<Integer>> freqMap;
+    int minFreq;
+    int capacity;
+
+    public LFU(int capacity) {
+        this.capacity = capacity;
+        cacheMap = new HashMap<>(capacity);
+        freqMap = new HashMap<>();
+    }
+
+    public int get(int key) {
+        if (cacheMap.get(key) == null || cacheMap.size() <= 0) return -1;
+        Node Node = cacheMap.get(key);
+        update(Node);
+        return Node.getValue();
+    }
+
+    public void put(int key, int value) {
+        //判断数据是否已经缓存
+        Node node = cacheMap.get(key);
+        //已经缓存，更新该数据
+        if (node != null) {
+            Node newNode = new Node(key, value,node.freq);
+            update(newNode);
+            cacheMap.put(key,newNode);
+        } else if (node == null && capacity != 0){
+            //未缓存，cache it判断容器容量
+            if (cacheMap.size() >= capacity) {
+                //容量不够，按照LFU删除一个数据
+                LinkedList<Integer> sameFreqList = freqMap.get(minFreq);
+                if (sameFreqList != null && sameFreqList.size() > 0) {
+                    Integer minKey = sameFreqList.pollFirst();
+                    cacheMap.remove(minKey);
+                }
+            }
+            //插入新数据
+            Node newNode = new Node(key, value,0);
+            update(newNode);
+            cacheMap.put(key,newNode);
+        }
+
+    }
+
+    private void update(Node node) {
+        int freq = 0;
+        if (node.getFreq() != 0) {
+            freq = node.getFreq();
+            LinkedList<Integer> sameFreqList = freqMap.get(freq);
+            sameFreqList.remove(node.getKey());
+            if (sameFreqList.size() == 0 && minFreq == freq) {
+                minFreq++;
+            }
+            freqMap.put(freq,sameFreqList);
+        } else {
+            minFreq = 1;
+        }
+
+        if (freqMap.get(freq + 1) == null) {
+            freqMap.put(freq + 1,new LinkedList<>());
+        }
+        LinkedList<Integer> nextList = freqMap.get(freq + 1);
+        nextList.addLast(node.getKey());
+        freqMap.put(freq + 1,nextList);
+
+        node.setFreq(freq + 1);
+    }
+    
+    class Node {
+        Integer key;
+        Integer value;
+        Integer freq;
+
+        public Node(Integer key, Integer value, Integer freq) {
+            this.key = key;
+            this.value = value;
+            this.freq = freq;
+        }
+
+        public Integer getKey() {
+            return key;
+        }
+
+        public void setKey(Integer key) {
+            this.key = key;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+
+        public Integer getFreq() {
+            return freq;
+        }
+
+        public void setFreq(Integer freq) {
+            this.freq = freq;
+        }
+    }
+}
--- LineSweep/MyTest.java
+++ LineSweep/MyTest.java
@@ -25,12 +25,58 @@ public class MyTest {
 //        int[] data = new int[]{-1,2147483647};
 //        boolean b = duplicateElements.containsNearbyAlmostDuplicate(data, 1, 2147483647);
 //        System.out.println(b);
-        ExamRoom room = new ExamRoom(10);
-        room.seat();
-        room.seat();
-        room.seat();
-        room.seat();
-        room.leave(4);
-        room.seat();
+
+//        ExamRoom room = new ExamRoom(10);
+//        room.seat();
+//        room.seat();
+//        room.seat();
+//        room.seat();
+//        room.leave(4);
+//        room.seat();
+
+        LFU lfu = new LFU(2);
+        lfu.put(1,1);
+        lfu.put(2,2);
+        System.out.println(lfu.get(1));
+        lfu.put(3,3);
+        System.out.println(lfu.get(2));
+        System.out.println(lfu.get(3));
+        lfu.put(4,4);
+        System.out.println(lfu.get(1));
+        System.out.println(lfu.get(3));
+        System.out.println(lfu.get(4));
+
+//        LFU lfu1 = new LFU(0);
+//        lfu1.put(0,0);
+//        System.out.println(lfu1.get(0));
+
+        /*LFU lfu1 = new LFU(1);
+        lfu1.put(2,1);
+        System.out.println(lfu1.get(2));
+        lfu1.put(3,2);
+        System.out.println(lfu1.get(2));
+        System.out.println(lfu1.get(3));*/
+        //
+        //1
+
+        //441 112
+        //1 -1 3
+
+//
+//
+//        LFU lfu = new LFU(3);
+//        lfu.put(2,2);
+//        lfu.put(1,1);
+//        System.out.println(lfu.get(2));
+//        System.out.println(lfu.get(1));
+//        System.out.println(lfu.get(2));
+//        lfu.put(3,3);
+//        lfu.put(4,4);
+//        System.out.println(lfu.get(3));
+//        System.out.println(lfu.get(2));
+//        System.out.println(lfu.get(1));
+//        System.out.println(lfu.get(4));
+//        // 331 112 223
+//        //2 1 2
     }
 }
